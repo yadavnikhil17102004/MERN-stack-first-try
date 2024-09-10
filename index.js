5828fd45f8a354a2ad83b36f580f4b33e41c0805
yadavnikhil17102004 <yadavnikhil17102004@gmail.com>
2024-09-10 16:27:15 +0530

    Connected mongodb database

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
-const indexRouter = require('./routes/index')
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname+'/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+const indexRouter = require('./routes/index');
 
-app.use('/', indexRouter)
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000)
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected to mongoose'));
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
